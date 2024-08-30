5ff76828deab30117f2aa84bc596a717c5711772
Sparsh Amitbhai Ladani <sp835101@dal.ca>
2024-08-30 10:41:08 -0300

    Fixed user authentication issues

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,19 +1,18 @@
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google';
 import { connectToDB } from "@utils/database";
-import user from "@models/user";
+import User from "@models/user";  
 
 const options = {
   providers: [
-    // OAuth authentication providers...
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET  
     }),
   ],
   callbacks: {
     async session({ session }) {
-      const sessionUser = await user.findOne({ email: session.user.email });
+      const sessionUser = await User.findOne({ email: session.user.email });
 
       session.user.id = sessionUser._id.toString();
 
@@ -23,10 +22,10 @@ const options = {
       try {
         await connectToDB();
 
-        const userExists = await user.findOne({ email: profile.email });
+        const userExists = await User.findOne({ email: profile.email });
 
         if (!userExists) {
-          await user.create({
+          await User.create({
             email: profile.email,
             username: profile.name.replace(" ", "").toLowerCase(),
             image: profile.picture
